47929e825d02f9ae0836731ac1919a716600c72c
Uno <uno.larsson@chas.se>
2011-12-08 17:34:48 +0100

    Rearranged code, and fixed some basic styles. The Page and Banner controllers no longer have code in the SecuredController.

--- src/Chas/AdminBundle/Controller/BannerController.php
+++ src/Chas/AdminBundle/Controller/BannerController.php
@@ -8,9 +8,108 @@ use Symfony\Component\HttpFoundation\Request;
 
 class BannerController extends Controller
 {
-    public function getPageAction($page, $form, $id = null)
+    public function bannerAction()
     {
-        switch($page)
+        $em = $this->getDoctrine()->getEntityManager();
+        $b = $em->getRepository('ChasBannerBundle:Banner')->findAll();
+        
+        $menus = $this->getMenu();
+
+        return $this->render('ChasAdminBundle:Admin:banner.html.twig', array('currentpage' => 'welcome','banners' => $b, 'form' => null, 'menus' => $menus));
+
+    }
+    
+    public function bannerNewAction(Request $request)
+    {
+        $b = new Banner();
+        $b->setName('Banner Name');
+        $b->setUrl('http://example.com');
+        $b->setClicks(0);
+        
+        $menus = $this->getMenu();
+
+        $form = $this->createFormBuilder($b)
+            ->add('name','text')
+            ->add('url','text')
+            ->add('pubdate', 'date')
+            ->add('unpubdate', 'date')
+            ->add('file')
+            ->add('clicks','hidden')
+            ->getForm();
+        
+        if ($request->getMethod() == 'POST'){
+            $form->bindRequest($request);
+
+            if ($form->isValid()){
+                $em = $this->getDoctrine()->getEntityManager();
+
+                $b->upload();
+
+                $em->persist($b);
+                $em->flush();
+
+                return $this->redirect($this->generateUrl('ChasAdminBundle_banner'));
+            }
+        }
+
+        return $this->render('ChasAdminBundle:Admin:banner.html.twig', array('currentpage' => 'new', 'form' => $form->createView(), 'menus' => $menus));
+    }
+
+    public function bannerUpdateAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $b = $em->getRepository('ChasBannerBundle:Banner')->find($id);
+        
+        $menus = $this->getMenu();
+
+        if (!$b && $request->getMethod() != 'POST'){
+            throw $this->createNotFoundException('No banner found for id ' . $id);
+        }
+        
+        $form = $this->createFormBuilder($b)
+            ->add('name','text')
+            ->add('url','text')
+            ->add('pubdate', 'date')
+            ->add('unpubdate', 'date')
+            ->add('file')
+            ->getForm();
+        
+        if ($request->getMethod() == 'POST'){
+            $form->bindRequest($request);
+
+            if ($form->isValid()){
+                
+                $b->upload();
+
+                $em->flush();
+
+                return $this->redirect($this->generateUrl('ChasAdminBundle_banner'));
+            }
+        }
+        
+        return $this->render('ChasAdminBundle:Admin:banner.html.twig', array('currentpage' => 'update', 'form' => $form->createView(), 'id' => $id, 'menus' => $menus));
+    }
+    
+    public function bannerRemoveAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $b = $em->getrepository('ChasBannerBundle:Banner')->find($id);
+        
+        $em->remove($b);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('ChasAdminBundle_banner'));
+
+    }
+
+
+    
+    
+    
+    
+    public function getPageAction($currentpage, $form, $id = null)
+    {
+        switch($currentpage)
         {
             case 'welcome':
                 return $this->welcome();
@@ -24,22 +123,37 @@ class BannerController extends Controller
         }
     }
 
-    public function welcome()
+    private function welcome()
     {
         $em = $this->getDoctrine()->getEntityManager();
         $banners = $em->getRepository('ChasBannerBundle:Banner')
             ->findAll();
-        return $this->render('ChasAdminBundle:Banner:banners.html.twig', array('banners' => $banners));
+        return $this->render('ChasAdminBundle:Banner:banners.html.twig', array('banners' => $banners, 'page' => 'welcome'));
     }
 
-    public function newBanner($form)
+    private function newBanner($form)
     {
         return $this->render('ChasAdminBundle:Banner:new.html.twig', array('form' => $form));
     }
 
-    public function updateBanner($form, $id)
+    private function updateBanner($form, $id)
     {
         return $this->render('ChasAdminBundle:Banner:update.html.twig', array('form' => $form, 'id' => $id));
+    }
+
+    public function getMenu()
+    {
+        $menu = array();
+
+        $banners_item['path'] = $this->generateUrl('ChasAdminBundle_banner');
+        $banners_item['name'] = 'Banners';
+        $menu[] = $banners_item;
+
+        $new_item['path'] = 'ChasAdminBundle_banner_new';
+        $new_item['name'] = 'New';
+        $menu[] = $new_item;
+
+        return $menu;
     }
 
 }
--- src/Chas/AdminBundle/Controller/PageController.php
+++ src/Chas/AdminBundle/Controller/PageController.php
@@ -8,29 +8,83 @@ use Symfony\Component\HttpFoundation\Request;
 
 class PageController extends Controller
 {
-    public function menuAction($page)
+    
+    public function pageAction()
+    {
+        $menus = $this->getMenu();
+        return $this->render('ChasAdminBundle:Admin:pages.html.twig', array('currentpage' => 'welcome', 'form' => null, 'menus' => $menus));
+    }
+   
+    public function pageNewAction(Request $request)
+    {
+        
+        $p = new Page();
+        $p->setPage('Page Title');
+        $p->setPhonenumber('0684-123 45');
+        $p->setEmail('hello@domain.nu');
+        $p->setAddress('Rörosvägen 124, 840 96 Funäsdalen');
+
+        $form = $this->createFormBuilder($p)
+            ->add('page','text')
+            ->add('phonenumber','text')
+            ->add('email','email')
+            ->add('address','textarea')
+            ->getForm();
+        
+        if ($request->getMethod() == 'POST'){
+            $form->bindRequest($request);
+
+            if ($form->isValid()){
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($p);
+                $em->flush();
+
+                return $this->redirect($this->generateUrl('ChasAdminBundle_page'));
+            }
+        }
+        return $this->render('ChasAdminBundle:Admin:pages.html.twig', array('currentpage' => 'new', 'form' => $form->createView(), 'menus' => $this->getMenu()));
+    }
+
+    public function pageUpdateAction($id, Request $request)
     {
         $em = $this->getDoctrine()->getEntityManager();
-        $query = $em->createQuery(
-            'SELECT p FROM ChasAPIBundle:Page p'
-        );
-        $r = $query->getResult();
+        $p = $em->getRepository('ChasAPIBundle:Page')->find($id);
         
-        return $this->render('ChasAdminBundle:Page:menu.html.twig', array('currentpage' => $page, 'pages' => $r));
+        if (!$p && $request->getMethod() != 'POST'){
+            throw $this->createNotFoundException('No page found for id ' . $id);
+        }
+        
+        $form = $this->createFormBuilder($p)
+            ->add('page','text')
+            ->add('phonenumber','text')
+            ->add('email','email')
+            ->add('address','textarea')
+            ->getForm();
+        
+        if ($request->getMethod() == 'POST'){
+            $form->bindRequest($request);
+
+            if ($form->isValid()){
+                $em->flush();
+
+                return $this->redirect($this->generateUrl('ChasAdminBundle_page'));
+            }
+        }
+        return $this->render('ChasAdminBundle:Admin:pages.html.twig', array('currentpage' => 'update','form' => $form->createView(), 'menus' => $this->getMenu()));
     }
     
-    public function getPageAction($page, $form)
+    public function getPageAction($currentpage, $form, $id = null)
     {
-        switch($page)
+        switch($currentpage)
         {
             case 'welcome':
                 return $this->welcome();
                 break;
-            case 'newPage':
+            case 'new':
                 return $this->newPage($form);
                 break;
-            case 'updatePage':
-                return $this->updatePage($form);
+            case 'update':
+                return $this->updatePage($form, $id);
                 break;
             default:
                 return $this->welcome();
@@ -47,9 +101,33 @@ class PageController extends Controller
         return $this->render('ChasAdminBundle:Page:new.html.twig', array('form' => $form));
     }
 
-    public function updatePage($form)
+    public function updatePage($form, $id)
     {
-        return $this->render('ChasAdminBundle:Page:update.html.twig', array('form' => $form));
+        return $this->render('ChasAdminBundle:Page:update.html.twig', array('form' => $form, 'id' => $id));
     }
+
+    public function getMenu()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $query = $em->createQuery(
+            'SELECT p FROM ChasAPIBundle:Page p'
+        );
+        $r = $query->getResult();
+        
+        $menu = array();
+        
+        for ($i=0;$i<count($r);$i++){
+            $banners_item['path'] = $this->generateUrl('ChasAdminBundle_page_update', array('id' => $r[$i]->getId() ));
+            $banners_item['name'] = $r[$i]->getPage();
+            $menu[] = $banners_item;
+        }
+
+        $new_item['path'] = $this->generateUrl('ChasAdminBundle_page_new');
+        $new_item['name'] = 'New';
+        $menu[] = $new_item;
+
+        return $menu;
+    }
+    
 
 }
